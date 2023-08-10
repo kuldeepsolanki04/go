f73740867bdb2b521c2f6a2dc728b5b9b8685c99
kuldeepsolanki04 <133967625+kuldeepsolanki04@users.noreply.github.com>
2023-08-10 20:23:40 +0530

    Update arena_test.go

--- src/arena/arena_test.go
+++ src/arena/arena_test.go
@@ -1,7 +1,7 @@
 // Copyright 2022 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+// test comment
 //go:build goexperiment.arenas
 
 package arena_test
